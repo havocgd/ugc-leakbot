9b15e4c4e96d8a75f95f8d3ec74317fc14a23360
havocgd <havocgdash@Gmail.com>
2025-09-29 16:26:45 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("UGC Leak Bot starting up...");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is alive!'));
+app.listen(3000, () => console.log('Web server running on port 3000'));
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
